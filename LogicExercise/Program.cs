e5d6ddd61ad910002f4dfaa745abdbf8fdb3e655
Vinsensius Wijaya <me.vinsensiuswijaya@gmail.com>
2025-05-13 11:18:40 +0700

    Logic Exercise

---
+++ LogicExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Input Number: ");
+int number = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0) Console.Write("foobar ");
+    else if (i % 3 == 0) Console.Write("foo");
+    else if (i % 5 == 0) Console.Write("bar");
+    else Console.Write(i);
+    if (i == number) break;
+    Console.Write(", ");
+}
