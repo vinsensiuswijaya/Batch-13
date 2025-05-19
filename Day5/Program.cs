cbb15c9db8fe283f94b05cfabcd103a11da8f9ba
Vinsensius Wijaya <me.vinsensiuswijaya@gmail.com>
2025-05-19 11:07:52 +0700

    Coba func delegate

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -16,32 +16,32 @@
             Console.WriteLine(result);
             return result;
         }
+        void PrintAll(List<int> nums)
+        {
+            foreach (int num in nums)
+            {
+                Console.Write($"{num} ");
+            }
+            Console.WriteLine();
+        }
+        
 
         // Demo Func
         List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
         Func<int, bool> isDivisibleByThree = num => num % 3 == 0;
-        Func<int, int> multipleByFifteen = num => num * 15;
+        Func<int, int> multipleByFive = num => num * 5;
+        Func<int, bool> isDivisibleByFifteen = num => num % 15 == 0;
 
         List<int> num2 = numbers.Where(isDivisibleByThree).ToList();
-        foreach (int num in num2)
-        {
-            Console.Write($"{num} ");
-        }
-        Console.WriteLine();
+        PrintAll(num2);
 
-        List<int> num3 = numbers.Select(multipleByFifteen).ToList();
-        foreach (int num in num3)
-        {
-            Console.Write($"{num} ");
-        }
-        Console.WriteLine();
+        List<int> num3 = numbers.Select(multipleByFive).Where(isDivisibleByFifteen).ToList();
+        PrintAll(num3);
 
         // Demo multicast delegate
         Transformer t = Square;
         t += PlusThree;
         int result = t(4);
         Console.WriteLine($"result: {result}");
-
-
     }
 }
