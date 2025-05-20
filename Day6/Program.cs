977ea535cb05b56c2441ee75fb3ebf4fc2feb8d3
Vinsensius Wijaya <me.vinsensiuswijaya@gmail.com>
2025-05-20 10:43:43 +0700

    Coba enumerator & iterator

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -54,5 +54,40 @@ class Program
         s1.Price = 200m;
         s1.PriceChanged -= subscriber.OnPriceChanged;
         s1.Price = 300m; // No output as the subscriber has unsubscribed
+
+        // Demo Enumerator & Iterator
+        IEnumerable<int> Fibs(int fibCount)
+        {
+            int prevFib = 0, curFib = 1;
+            for (int i = 0; i < fibCount; i++)
+            {
+                yield return prevFib;
+                int newFib = prevFib + curFib;
+                prevFib = curFib;
+                curFib = newFib;
+            }
+        }
+
+        foreach (int fib in Fibs(6))
+        {
+            Console.Write($"{fib} ");
+        }
+        Console.WriteLine();
+
+        IEnumerable<int> Triangular(int count)
+        {
+            int prev = 0, cur = 1;
+            for (int i = 0; i < count; i++)
+            {
+                yield return prev;
+                prev += cur;
+                cur++;
+            }
+        }
+
+        foreach (int sequence in Triangular(6))
+        {
+            Console.Write($"{sequence} ");
+        }
     }
 }
