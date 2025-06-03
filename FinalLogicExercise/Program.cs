09baed4d5475e0f84c59395070cd714951d8a7c6
Vinsensius Wijaya <me.vinsensiuswijaya@gmail.com>
2025-06-03 08:53:03 +0700

    Final Logic Exercise

---
+++ FinalLogicExercise/Program.cs
@@ -0,0 +1,52 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Generator g1 = new Generator(new Dictionary<int, string>
+        {
+            [3] = "foo",
+            [4] = "baz",
+            [5] = "bar",
+            [7] = "jazz",
+            [9] = "huzz"
+        });
+        g1.AddRule(11, "har");
+        Console.Write("Input number: ");
+        int number = int.Parse(Console.ReadLine());
+        g1.Print(number);
+    }
+}
+
+public class Generator
+{
+    public Dictionary<int, string> Rules { get; set; }
+    public Generator()
+    {
+        Rules = new Dictionary<int, string>();
+    }
+    public Generator(Dictionary<int, string> rules)
+    {
+        Rules = rules;
+    }
+    public void AddRule(int input, string output)
+    {
+        Rules[input] = output;
+    }
+    public void Print(int number)
+    {
+        for (int num = 1; num <= number; num++)
+        {
+            bool isChecked = false;
+            foreach (var rule in Rules)
+            {
+                if (num % rule.Key == 0)
+                {
+                    Console.Write($"{rule.Value}");
+                    isChecked = true;
+                }
+            }
+            if (!isChecked) Console.Write($"{num}");
+            if (num != number) Console.Write(", ");
+        }
+    }
+}
